006d53856772ef8b37a3cfad525318371f3ccac3
William <will@vinova.sg>
2012-08-24 16:44:15 +0700

    Initial Commit

---
+++ ratings2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ratings2
+//
+//  Created by William on 8/24/12.
+//  Copyright (c) 2012 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
